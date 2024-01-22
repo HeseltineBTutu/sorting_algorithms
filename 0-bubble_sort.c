a68a3c7345e46c4924dc14fc70cee72a7da60d04
HeseltineBTutu <heseltinetutu@gmail.com>
2024-01-22 07:59:33 -0800

    Implement Bubble Sort with Swap Count and Array Printing

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+/**
+ * bubble_sort - a function that sorts an array of integers in
+ *               ascending order using the Bubble sort algorithm
+ * @array: array to be sorted
+ * size: size of the array
+ *
+ * Return: Returns nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+	bool swapped;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = true;
+
+				for (k = 0; k < size; k++)
+				{
+					printf("%d ", array[k]);
+				}
+				printf("\n");
+			}
+		}
+		if (!swapped)
+			break;
+	}
+}
