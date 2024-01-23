d18fc3a47fb9811d5fd5f4b962b4d12d5da9c533
HeseltineBTutu <heseltinetutu@gmail.com>
2024-01-23 12:53:01 -0800

    Refactor selection_sort function Optimized to avoid unnecessary swaps when the minimum element is already in its correct position

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -27,10 +27,13 @@ void selection_sort(int *array, size_t size)
 			}
 		}
 
-		temp = array[minIndex];
-		array[minIndex] = array[i];
-		array[i] = temp;
-
-		print_array(array, size);
+		if (minIndex != i)
+		{
+			temp = array[minIndex];
+			array[minIndex] = array[i];
+			array[i] = temp;
+			
+			print_array(array, size);
+		}
 	}
 }
