a2995ab8fd92b52f4784cd051768b7d31e46ad1c
HeseltineBTutu <heseltinetutu@gmail.com>
2024-01-24 10:02:35 -0800

    Refactor function quick_sort Display step-by-step array states during sorting

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,20 +1,23 @@
 #include "sort.h"
 
 /**
- * lomuto_partition - Lomuto partition scheme
- * @array: Array to be partitioned
+ * lomuto_partition - Order a subset of an array of integers according to
+ *                    the lomuto partition scheme (last element as pivot).
+ * @array: Array of integers.
  * @size: Size of the array
+ * @left: The starting index of the subset to order.
+ * @right: The ending index of the subset to order.
  *
- * Return: Nothing(void)
+ * Return: The final partition index
  */
-size_t lomuto_partition(int *array, size_t size)
+size_t lomuto_partition(int *array, size_t size, int left, int right)
 {
 	int temp;
-	size_t j;
-	int pivot = array[size - 1];
-	int i = 0;
+	int j;
+	int pivot = array[right];
+	int i = left;
 
-	for (j = 0; j < size - 1; j++)
+	for (j = left; j < right; j++)
 	{
 		if (array[j] <= pivot)
 		{
@@ -29,8 +32,8 @@ size_t lomuto_partition(int *array, size_t size)
 	}
 	/* Swap array[i + 1] and array[high] to place pivot in the correct position*/
 	temp = array[i];
-	array[i] = array[size - 1];
-	array[size - 1] = temp;
+	array[i] = array[right];
+	array[right] = temp;
 	print_array(array, size);
 
 	return (i);
@@ -46,11 +49,12 @@ void quick_sort(int *array, size_t size)
 {
 	size_t pivot_index;
 
-	if (size <= 1)
+	if (array == NULL || size < 2)
 		return;
 
-	pivot_index = lomuto_partition(array, size);
+	pivot_index = lomuto_partition(array, size, 0, size - 1);
 
-	quick_sort(array, pivot_index);
-	quick_sort(array + pivot_index +  1, size - pivot_index - 1);
+	if (pivot_index > 0)
+		quick_sort(array, pivot_index);
+	quick_sort(array + pivot_index +  1, size - (pivot_index + 1));
 }
--- sort.h
+++ sort.h
@@ -21,6 +21,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-size_t lomuto_partition(int *array, size_t size);
+size_t lomuto_partition(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
 #endif
